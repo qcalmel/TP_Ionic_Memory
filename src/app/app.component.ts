24bae8a2e43deb5fb3305de8f0e3a98478ca7404
qcalmel <contact@quentincalmel.fr>
2022-04-20 20:09:36 +0200

    feat(app) : add title browser tap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import {Title} from '@angular/platform-browser';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,12 +15,14 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private title: Title
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    this.title.setTitle('MemoBean');
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
